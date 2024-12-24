e9c6571cc6cb8036df061de4754264208bbc34d7
m0stafa7med <mostafadarwesh610@gmail.com>
2024-12-24 21:06:27 +0200

    Change frontend auth config

--- frontend/youtube-clone-ui/src/app/auth/auth-config.module.ts
+++ frontend/youtube-clone-ui/src/app/auth/auth-config.module.ts
@@ -5,9 +5,9 @@ import { AuthModule } from 'angular-auth-oidc-client';
 @NgModule({
     imports: [AuthModule.forRoot({
         config: {
-            authority: 'airbnb-backend.us.auth0.com',
+            authority: 'https://dev-saq1mdhwlx2xddtx.us.auth0.com',
             redirectUrl: window.location.origin,
-            clientId: 'aGVjoVb9banVCllgYJ7oeeMID3Gz6Qhs',
+            clientId: 'MUgArEJ5rwfxKbHbXgjWAFFxAGYbuy5G',
             scope: 'openid profile offline_access',
             responseType: 'code',
             silentRenew: true,
