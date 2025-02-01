a433ef68eb9229cd6a12cfd7a1704d5ae1eb3aff
m0stafa7med <mostafadarwesh610@gmail.com>
2025-02-01 19:06:43 +0200

    upgrade to v18

--- frontend/youtube-clone-ui/src/app/app.module.ts
+++ frontend/youtube-clone-ui/src/app/app.module.ts
@@ -6,7 +6,7 @@ import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {UploadVideoComponent} from './upload-video/upload-video.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {HttpClientModule} from "@angular/common/http";
+import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
 import {NgxFileDropModule} from "ngx-file-drop";
 import {MatButtonModule} from "@angular/material/button";
 import {MatToolbarModule} from "@angular/material/toolbar";
@@ -31,20 +31,17 @@ import {AuthConfigModule} from "./auth/auth-config.module";
 import {MatDividerModule} from "@angular/material/divider";
 import {MatCardModule} from "@angular/material/card";
 
-@NgModule({
-    declarations: [
+@NgModule({ declarations: [
         AppComponent,
         UploadVideoComponent,
         SaveVideoDetailsComponent,
         VideoPlayerComponent,
         VideoDetailComponent
     ],
-    imports: [
-        BrowserModule,
+    bootstrap: [AppComponent], imports: [BrowserModule,
         AppRoutingModule,
         BrowserAnimationsModule,
         FormsModule,
-        HttpClientModule,
         NgxFileDropModule,
         MatButtonModule,
         MatToolbarModule,
@@ -65,10 +62,6 @@ import {MatCardModule} from "@angular/material/card";
         HeaderComponent,
         AuthConfigModule,
         MatDividerModule,
-        MatCardModule
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
-})
+        MatCardModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule {
 }
